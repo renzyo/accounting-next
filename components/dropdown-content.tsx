37dd85e2c6a2fbd4e95d172a358f7056b1dca5b7
Merza Bolivar <merza.bolivar@gmail.com>
2023-09-16 12:45:58 +0700

    fix: logout not working

--- components/dropdown-content.tsx
+++ components/dropdown-content.tsx
@@ -23,11 +23,11 @@ export default function DropdownContent({ name }: DropdownContentProps) {
 
   async function handleLogout() {
     try {
-      toast.success("Logged out");
       const response = await axios.get("/api/auth/logout");
 
       if (response.status === 200) {
-        window.location.assign("/");
+        toast.success("Logged out");
+        window.location.assign("/en/login");
       }
     } catch (error) {
       toast.error("Something went wrong");
